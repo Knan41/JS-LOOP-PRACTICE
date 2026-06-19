8129825650736965bda533230e6d9f974bed0b9c
Knan41 <knanbayramov41@gmail.com>
2026-06-19 08:18:06 +0400

    loop

---
+++ script.js
@@ -0,0 +1,35 @@
+//   Task 1:
+//   1-dən Verilmiş n ədədinə qədər vurma cədvəlini çıxart
+
+// let n = +prompt('enter your number');
+
+// for (i = 1; i <= n; i++) {
+//     for (k = 1; j <= 10; j++) {
+//         console.log(i + " x " + j + " = " + (i * k));
+//     }
+// }
+
+
+//  Task2:
+//  while istifadə edərək 20-dən 1-ə qədər geriyə say ,
+
+// let i = 20;
+
+// while (i >= 1) {
+//   console.log(i);
+//   i--;
+// }
+
+
+//   task 3
+// 2 və 5-ə bölünən ədədləri konsolda çap et.
+
+let a = 100
+
+while(a >= 1){
+    if(a%2 ===0 && a%5 ===0){
+        console.log(a);
+
+    } 
+    a--;
+}
